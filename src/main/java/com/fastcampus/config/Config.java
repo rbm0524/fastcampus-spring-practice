d4b8f056fece7729a90b6b605855eb9c450d5a46
BOMIN LYU <rbm0524@naver.com>
2024-08-19 22:09:29 +0900

    Apply #1 - spring configuration
    기초 설정
    루트 패키지 이하의 빈들을 스캔한다.

---
+++ src/main/java/com/fastcampus/config/Config.java
@@ -0,0 +1,11 @@
+package com.fastcampus.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fastcampus") //이 패키지 이하의 모든 Bean을 스캔한다.
+@Configuration //Spring 전용 Configuration 클래스로 지정 -> AOP
+public class Config {
+
+
+}
